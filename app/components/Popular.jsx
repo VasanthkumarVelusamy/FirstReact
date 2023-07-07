c89b3899a2a9571a371fb3fcc2facb0440283fb6
Vasanth Kumar <vasanthkumar.work@gmail.com>
2023-07-07 19:41:46 +0530

    Adding Popular component to show the dropdown menu with popular languages

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,16 +1,35 @@
 import * as React from 'react'
 
 export default class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedLanguage: "All"
+        }
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+
+    updateLanguage(language) {
+        this.setState({
+            selectedLanguage: language
+        })
+    }
     render() {
         const languages = ["All", "JavaScript", "Ruby", "HTML", "CSS", "Python"]
         return (
-            <select>
-                {
-                    languages.map((language) =>
-                        <option key={language} value={language}>{language}</option>
-                    )
-                }
-            </select>
+            <main>
+                <select
+                    onChange={(e) => this.updateLanguage(e.target.value)}
+                    selected={this.state.selectedLanguage}
+                >
+                    {
+                        languages.map((language) =>
+                            <option key={language} value={language}>{language}</option>
+                        )
+                    }
+                </select>
+                {JSON.stringify(this.state, null, 2)}
+            </main>
         )
     }
 }
